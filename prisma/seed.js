0ecf02dac4362a5089a60a7c7d927059f4dc96e6
Austin.Dinsmore <austdins@DESKTOP-C5NFOS3>
2024-08-05 19:01:11 -0600

    seed file fully working

--- prisma/seed.js
+++ prisma/seed.js
@@ -4,9 +4,10 @@ const {faker} = require("@faker-js/faker");
 const prisma = new PrismaClient();
 
 const main = async () =>{
+    console.log("Creating users");
     await prisma.$connect();
     const [user1, user2, user3, user4, user5] = await Promise.all(
-        [...Array(5)].map(async () => {
+        [...Array(5)].map(() => {
         return prisma.users.create({
             data: {
                 username: faker.internet.userName(),
@@ -17,58 +18,50 @@ const main = async () =>{
     );
     const users = await prisma.users.findMany();
     console.log("Created users:", users);
-
-    console.log("Creating comments");
-    await Promise.all(
-        [user1, user2, user3, user4, user5].map((users) => {
-            [...Array(1)].map(async () => {
-                return prisma.comments.create({
-                    data: {
-                        comment: faker.lorem.sentences({min: 1, max: 3}),
-                        author_id: author.id,
-                        review_id: reviews.id,
-                    },
-                })
-            })
-        })
-    );
-    const comments = await prisma.comments.findMany();
-    console.log("Created comments:", comments);
-
-    console.log("Creating Reviews", reviews);
-    await Promise.all(
-        [user1, user2, user3, user4, user5].map((users) => {
-            [...Array(1)].map(async () => {
-                return prisma.reviews.create({
-                    data: {
-                        score: '',
-                        txt: faker.lorem.sentence(),
-                        user_id: user1.id,
-                        item_id: items.id,
-                    }
-                })
-            })
-        })
-    );
-    const reviews = await prisma.reviews.findMany();
-    console.log("Created reviews:", reviews);
-
-    console.log("Creating Items", items);
-    await Promise.all(
-        [user1, user2, user3, user4, user5].map((users) => {
-            [...Array(1)].map(async () => {
-                return prisma.reviews.create({
+    
+    console.log("Creating Items:");
+    const [item1, item2, item3] = await Promise.all(
+            [...Array(5)].map(() => prisma.items.create({
                     data: {
                         name: faker.company.name(),
                         description: faker.lorem.sentences({min: 1, max: 3}),
                     }
                 })
-            })
-        })
+        )
     );
     const items = await prisma.items.findMany();
     console.log("Created Items", items);
+
+    console.log("Creating Reviews");
+    const [review1, review2, review3, review4] = await Promise.all(
+            [...Array(5)].map((_,i) => prisma.reviews.create({
+                    data: {
+                        score: faker.number.float({ multipleOf: 0.50, min: 0, max:5 }),
+                        txt: faker.lorem.sentence(),
+                        user_id: users[i].id,
+                        item_id: items[i].id,
+                    }
+                }))
+    );
+
+    const reviews = await prisma.reviews.findMany();
+    console.log("Created reviews:", reviews);
+
+console.log("Creating comments");
+const [comment1, comment2, comment3, comment4] = await Promise.all(
+        [...Array(5)].map((_,i) => prisma.comments.create({
+                data: {
+                    comment: faker.lorem.sentences({min: 1, max: 3}),
+                    author_id: users[i].id,
+                    review_id: reviews[i].id,
+                },
+            })
+    )
+);
+const comments = await prisma.comments.findMany();
+console.log("Created comments:", comments);
 };
+
 
 main().then(async () =>{
     await prisma.$disconnect();
